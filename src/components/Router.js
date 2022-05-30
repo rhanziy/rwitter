d3514030d7c2f3650012ee54660ba6a268c3b40a
rhanziy <thdfksdl0029@gmail.com>
2022-05-31 00:16:35 +0900

    add Auth

---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+
+const AppRouter = ({isLoggedIn}) => {
+  
+  return(
+    <Router>
+      <Routes>
+        {isLoggedIn ? 
+            <>
+            <Route path="/" element={<Home />}></Route>
+            </>
+          : 
+          <Route path="/" element={<Auth />}></Route>
+        }
+      </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter;
---
+++ src/routes/Auth.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange =(event) =>{
+    const {target: {name, value}} = event;
+    if(name === "email") {
+      setEmail(value);
+    }
+    if(name === "password"){
+      setPassword(value);
+    }
+  }
+  const onSubmit =(event) =>{
+    event.preventDefault();
+  }
+  return (
+    <div> 
+      <form onSubmit={onSubmit}>
+        <input 
+          name="email" 
+          type="email" 
+          placeholder="Email" 
+          required 
+          value={email}
+          onChange={onChange}          
+        >
+        </input>
+        <input 
+          name="password" 
+          type="password" 
+          placeholder="Password" 
+          required 
+          value={password}
+          onChange={onChange}                 
+        >
+        </input>
+        <input type="submit" value="Log In"></input>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+}
+
+
+export default Auth;
