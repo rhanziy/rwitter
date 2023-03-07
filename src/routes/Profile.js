1fa20cdf662a554ad5f00f2ede76fc1b8ec23a72
rhanziy <thdfksdl0029@gmail.com>
2023-03-07 19:04:58 +0900

    edit profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,17 +1,31 @@
-import React from "react";
-import { authService } from "../myBase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "../myBase";
 import { useHistory } from "react-router-dom";
+import { query, collection, onSnapshot, where, getDoc } from "@firebase/firestore";
 
 
 
-
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const history = useHistory();
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/")
     }
-    
+    const getMyRweets = async() => {
+        const q = query(
+            collection(dbService, "rweets"),
+            where("creatorId", "==", `${userObj.uid}`)
+        );
+        const unSubscribe = await onSnapshot(q, (snapshot) => {
+            const newArry = snapshot.docs.forEach((e)=>{
+                console.log(e.id, "=>", e.data());
+            })
+        });
+    }
+    useEffect(()=>{
+        getMyRweets();
+    }, [userObj])
+
     return(
         <>
             <button onClick={onLogOutClick}>
